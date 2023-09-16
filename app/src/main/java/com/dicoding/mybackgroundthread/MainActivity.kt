6547ef0c9d40f85660664f46f21464591798043e
Ilham Ramadhan <ilhamram332@gmail.com>
2023-09-16 14:49:23 +0700

    Add Excecutor and Handler

--- app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -1,46 +1,44 @@
 package com.dicoding.mybackgroundthread
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.dicoding.mybackgroundthread.ui.theme.MyBackgroundThreadTheme
+import java.util.concurrent.Executors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyBackgroundThreadTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        btnStart.setOnClickListener {
+            executor.execute {
+                try {
+                    //simulate process in background thread
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+                        handler.post {
+                            //update ui in main thread
+                            if (percentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyBackgroundThreadTheme {
-        Greeting("Android")
     }
 }
